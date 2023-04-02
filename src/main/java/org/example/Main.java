95ee5d1884d139065db0ed8e719ef81ca7966663
Chris Atchison <chris.atcho@gmail.com>
2023-04-02 12:42:24 +1000

    Went through progress, update README

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ import org.pcap4j.core.*;
 import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.packet.Packet;
 import org.pcap4j.util.NifSelector;
-import java.util.List;
-import java.util.Scanner;
+
+// This project intakes packets using pcap4j, and then breaks them down into hex values, to be reconstructed into a custom packet class that can be used to analyze the packet.
 
 public class Main {
     public static void main(String[] args) throws PcapNativeException, NotOpenException, InterruptedException {
@@ -14,31 +14,25 @@ public class Main {
         System.out.println(nif.getName());
 
         //Params for incoming packets
-//        int snapshotLength = 65536;
-//        int readTimeout = 50;
-//        final PcapHandle handle = networkInterface.openLive(snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, readTimeout);
-//
-//
-//        //Deals with the packets. More specifically, gotPacket() does
-//        PacketListener listener = new PacketListener() {
-//            @Override
-//            public void gotPacket(Packet packet) {
-//                EthPacket ep = new EthPacket(packet.getRawData());
-//                System.out.println(ep.tostring());
-//            }
-//        };
-//
-//        //Keeps the interface open and then uses the listener object to handle the packet
-//        handle.loop(0,listener);
+        int snapshotLength = 65536;
+        int readTimeout = 50;
 
-
+        // A handle is an abstraction of a pointer, referring to the interface.
+        try (PcapHandle handle = nif.openLive(snapshotLength, PcapNetworkInterface.PromiscuousMode.NONPROMISCUOUS , readTimeout)) {
+            handle.setFilter("ip", BpfProgram.BpfCompileMode.OPTIMIZE);
+            PacketListener listener = new PacketListener() {
+                @Override
+                public void gotPacket(Packet packet) {
+                    EthPacket ep = new EthPacket(packet.getRawData());
+                    System.out.println(ep.tostring());
+                }
+            };
+            handle.loop(0,listener);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error with handle.");
+        }
     }
-
-    /*
-    This function lists the available interfaces on the device, and then you choose one
-    This needs to be improved for invalid choice
-     */
-
     static PcapNetworkInterface getInterface() {
         PcapNetworkInterface device = null;
 
@@ -50,23 +44,4 @@ public class Main {
 
         return device;
     }
-//    static String getInterface() {
-//        try{
-//            List<PcapNetworkInterface> interfaces = Pcaps.findAllDevs();
-//
-//            System.out.print("Choose one of the available interfaces: ");
-//            for(PcapNetworkInterface p : interfaces)System.out.println(p.getName());
-//
-//            Scanner myObj = new Scanner(System.in);
-//            String nif = myObj.nextLine();
-//            return nif;
-//
-//        } catch (PcapNativeException e){
-//            System.out.println(e.getStackTrace());
-//        }
-//        return "Invalid Interface";
-//    }
-
-
-
 }

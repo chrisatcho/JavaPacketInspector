503bf9b292709e4b8addefb00e1148f314d42a17
Chris Atchison <chris.atcho@gmail.com>
2023-04-02 12:20:17 +1000

    Initial Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,35 @@
 package org.example;
 
+import java.io.IOException;
 import org.pcap4j.core.*;
+import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.packet.Packet;
-
+import org.pcap4j.util.NifSelector;
 import java.util.List;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws PcapNativeException, NotOpenException, InterruptedException {
-        String chosenInterface = getInterface();//Using user input choose an interface
-        PcapNetworkInterface networkInterface = Pcaps.getDevByName(chosenInterface);//Get the interface object
-
-        //PcapNetworkInterface networkInterface = Pcaps.getDevByName("en0");//Get the interface object
+        PcapNetworkInterface nif = getInterface(); //Get the interface object
+        System.out.println(nif.getName());
 
         //Params for incoming packets
-        int snapshotLength = 65536;
-        int readTimeout = 50;
-        final PcapHandle handle = networkInterface.openLive(snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, readTimeout);
-
-
-        //Deals with the packets. More specifically, gotPacket() does
-        PacketListener listener = new PacketListener() {
-            @Override
-            public void gotPacket(Packet packet) {
-                EthPacket ep = new EthPacket(packet.getRawData());
-                System.out.println(ep.tostring());
-            }
-        };
-
-        //Keeps the interface open and then uses the listener object to handle the packet
-        handle.loop(0,listener);
+//        int snapshotLength = 65536;
+//        int readTimeout = 50;
+//        final PcapHandle handle = networkInterface.openLive(snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, readTimeout);
+//
+//
+//        //Deals with the packets. More specifically, gotPacket() does
+//        PacketListener listener = new PacketListener() {
+//            @Override
+//            public void gotPacket(Packet packet) {
+//                EthPacket ep = new EthPacket(packet.getRawData());
+//                System.out.println(ep.tostring());
+//            }
+//        };
+//
+//        //Keeps the interface open and then uses the listener object to handle the packet
+//        handle.loop(0,listener);
 
 
     }
@@ -37,22 +38,34 @@ public class Main {
     This function lists the available interfaces on the device, and then you choose one
     This needs to be improved for invalid choice
      */
-    static String getInterface() {
-        try{
-            List<PcapNetworkInterface> interfaces = Pcaps.findAllDevs();
 
-            System.out.print("Choose one of the available interfaces: ");
-            for(PcapNetworkInterface p : interfaces)System.out.println(p.getName());
+    static PcapNetworkInterface getInterface() {
+        PcapNetworkInterface device = null;
 
-            Scanner myObj = new Scanner(System.in);
-            String nif = myObj.nextLine();
-            return nif;
-
-        } catch (PcapNativeException e){
-            System.out.println(e.getStackTrace());
+        try {
+            device = new NifSelector().selectNetworkInterface();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return "Invalid Interface";
+
+        return device;
     }
+//    static String getInterface() {
+//        try{
+//            List<PcapNetworkInterface> interfaces = Pcaps.findAllDevs();
+//
+//            System.out.print("Choose one of the available interfaces: ");
+//            for(PcapNetworkInterface p : interfaces)System.out.println(p.getName());
+//
+//            Scanner myObj = new Scanner(System.in);
+//            String nif = myObj.nextLine();
+//            return nif;
+//
+//        } catch (PcapNativeException e){
+//            System.out.println(e.getStackTrace());
+//        }
+//        return "Invalid Interface";
+//    }
 
 
 

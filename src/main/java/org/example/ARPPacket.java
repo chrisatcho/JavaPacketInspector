e6b7e9d84d9a5236c50d24052ac6270507e12c30
RoryO <zroryo@gmail.com>
2023-05-01 14:51:54 +1000

    Fixed debug output statement

--- src/main/java/org/example/ARPPacket.java
+++ src/main/java/org/example/ARPPacket.java
@@ -5,7 +5,6 @@ public class ARPPacket extends L3Packet{
 
     public ARPPacket(String s) {
         parseVariables(s);
-        System.out.println("_M_M_M__M_M_M_M_M_M_M_M_M_M_M_M_M_M_M_M_M__M_M_M_M_M_M_M_M_M_ ARP");
     }
 
     public void parseVariables(String s) {
